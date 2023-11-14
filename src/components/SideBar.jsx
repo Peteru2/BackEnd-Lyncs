984cc934546ad03812f760b1ba0339398862cb21
DevPeter <polalekan526@gmail.com>
2023-11-14 02:06:55 +0100

    Sidebar worked on

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,16 +1,10 @@
 import loggo from  "../images/logo.png"
 import DashIcon1 from "../images/category.svg"
-import DashIcon2 from "../images/element-3.svg"
 import PendOIcon1 from "../images/ShoppingCartSimple2.svg"
-import PendOIcon2 from "../images/ShoppingCartSimple.svg"
 import PendDIcon1 from "../images/Truck2.svg"
-import PendDIcon2 from "../images/Truck.svg"
 import AwaitIcon1 from "../images/Alarm2.svg"
-import AwaitIcon2 from "../images/Alarm.svg"
 import ClosedIcon1 from "../images/CalendarX2.svg"
-import ClosedIcon2 from "../images/CalendarX.svg"
 import user1 from "../images/UsersFour2.svg" 
-import user2 from "../images/UsersFour.svg" 
 import { useAuth } from './AuthContext';
 
 
@@ -23,37 +17,36 @@ const SideBar = () => {
             title: "Dashboard",
             url: "/",
             icon1: DashIcon1,
-            icon2: DashIcon2
         },
         {
             title: "Pending Orders",
             url: "/Pending",
             icon1: PendOIcon1,
-            icon2: PendOIcon2
+            
         },
         {
             title: "Pending Deliveries",
             url: "/Pend-Deliveries",
             icon1: PendDIcon1,
-            icon2: PendDIcon2
+            
         },
         {
             title: "Awaiting Feedbacks",
             url: "/Awaiting-Feedback",
             icon1: AwaitIcon1,
-            icon2: AwaitIcon2
+           
         },
         {
             title: "Closed Deliveries",
             url: "/Closed-Deliveries",
             icon1: ClosedIcon1,
-            icon2: ClosedIcon2
+            
         },
          {
             title: "Staff",
             url: "/Staff",
             icon1: user1,
-            icon2: user2
+           
         },
 
     ]
@@ -62,7 +55,7 @@ const SideBar = () => {
     const handleLogout = () =>{
        logout()
     }
-
+    
     return ( 
         <>
             <aside className="bg-white sideBar border-r-2">
@@ -75,7 +68,7 @@ const SideBar = () => {
                     {navSideList.map((item,index) =>{
                         return(
                     <div key={index} className={`flex text-sm pl-6 my-6 py-2 cursor-pointer  ${location.pathname === item.url ? "Sidebar-border" :""} `}>
-                      <Link to={item.url} className="flex items-center outline-none">  { location.pathname=== item.url ? (<img src={item.icon1} alt="l" className="fa fa-dashbord mr-5 "/>) :(<img src={item.icon2} alt="l" className="fa fa-dashbord mr-5 "/>)}<p className={location.pathname === item.url ? "text_color" :""}>{item.title}</p></Link>
+                      <Link to={item.url} className="flex items-center outline-none">     <img src={item.icon1} alt="l" className={` mr-5 ${location.pathname=== item.url?'active-icon':'inactive-icon'}` } style={{fill: "#F18701"}}/> <p className={location.pathname === item.url ? "text_color" :""}>{item.title}</p></Link>
                     </div>
 
                     )
