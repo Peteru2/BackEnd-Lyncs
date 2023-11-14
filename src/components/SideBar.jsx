32d3abe75754398a9599c5895c3df0c3a893ca74
DevPeter <polalekan526@gmail.com>
2023-11-14 12:06:15 +0100

    Sidebar worked on

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import loggo from  "../images/logo.png"
-import DashIcon1 from "../images/category.svg"
-import PendOIcon1 from "../images/ShoppingCartSimple2.svg"
-import PendDIcon1 from "../images/Truck2.svg"
-import AwaitIcon1 from "../images/Alarm2.svg"
-import ClosedIcon1 from "../images/CalendarX2.svg"
-import user1 from "../images/UsersFour2.svg" 
+import  DashSideIcon from "../images/DashSideIcon.jsx"
+import PendOrderSideIcon from "../images/ShoppingCartSimple2.jsx"
+import PendDiliSideIcon from "../images/Truck2.jsx"
+import AwaitSideIcon from "../images/Alarm2.jsx"
+import ClosedSideIcon from "../images/CalendarX2.jsx"
+import StaffSideIcon from "../images/UsersFour2.jsx" 
 import { useAuth } from './AuthContext';
 
 
@@ -16,36 +17,36 @@ const SideBar = () => {
         {
             title: "Dashboard",
             url: "/",
-            icon1: DashIcon1,
+            icon1: <DashSideIcon />,
         },
         {
             title: "Pending Orders",
             url: "/Pending",
-            icon1: PendOIcon1,
+            icon1: <PendOrderSideIcon />
             
         },
         {
             title: "Pending Deliveries",
             url: "/Pend-Deliveries",
-            icon1: PendDIcon1,
+            icon1: <PendDiliSideIcon />
             
         },
         {
             title: "Awaiting Feedbacks",
             url: "/Awaiting-Feedback",
-            icon1: AwaitIcon1,
+            icon1: <AwaitSideIcon />
            
         },
         {
             title: "Closed Deliveries",
             url: "/Closed-Deliveries",
-            icon1: ClosedIcon1,
+            icon1: <ClosedSideIcon />
             
         },
          {
             title: "Staff",
             url: "/Staff",
-            icon1: user1,
+            icon1: <StaffSideIcon />,
            
         },
 
@@ -66,9 +67,12 @@ const SideBar = () => {
             <div>
                 <div className=" text-gray-500 mt-12">
                     {navSideList.map((item,index) =>{
+                        const isActive = location.pathname === item.url;
                         return(
                     <div key={index} className={`flex text-sm pl-6 my-6 py-2 cursor-pointer  ${location.pathname === item.url ? "Sidebar-border" :""} `}>
-                      <Link to={item.url} className="flex items-center outline-none">     <img src={item.icon1} alt="l" className={` mr-5 ${location.pathname=== item.url?'active-icon':'inactive-icon'}` } style={{fill: "#F18701"}}/> <p className={location.pathname === item.url ? "text_color" :""}>{item.title}</p></Link>
+                      <Link to={item.url} className="flex items-center outline-none"> 
+                      <p className='mr-5'>{React.cloneElement(item.icon1, { isActive: isActive })}</p>
+                      <p className={location.pathname === item.url ? "text_color" :"red"}>{item.title}</p></Link>
                     </div>
 
                     )
